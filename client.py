d20a2e920d485a690c1a1f57b7de15efaf2c5faa
daniilwithangelic <121841013+daniilwithangelic@users.noreply.github.com>
2023-02-15 16:10:57 +1000

    Update client.py

--- client.py
+++ client.py
@@ -2,22 +2,32 @@ import asyncio
 import websockets
 import json
 
+
 async def hello():
     uri = "ws://localhost:8765"
     async with websockets.connect(uri) as websocket:
         while True:
-            message = json.dumps({"topic": 'name', "text": input("What's your name?")})
-            await websocket.send(message)
+            message = {"topic": 'name', "text": input(">>> Write your name: ")}
+            vsvod = json.dumps(message)
+            await websocket.send(vsvod)
+            v = message["text"]
+            print("<<<",v)
             greeting = await websocket.recv()
-            print(f"<<< {greeting}")
-            
-            message = json.dumps({"topic": 'propose', "text": input("Wanna go out?")})
-            
-            await websocket.send(message)
+            print(f">>> {greeting}")
 
+            message = {"topic": 'propose', "text": v, "vuv": input(">>> Wanna go out?")}
+            vsvod = json.dumps(message)
+            print("<<<",message["vuv"])
+            await websocket.send(vsvod)
             greeting = await websocket.recv()
-            print(f"<<< {greeting}")
-            
+            print(f">>> {greeting}")
+
+            message = {"topic": 'news', "text": v, "drop": input(">>> Want to know more?")}
+            vsvod = json.dumps(message)
+            print("<<<", message["drop"])
+            await websocket.send(vsvod)
+            greeting = await websocket.recv()
+            print(f">>> {greeting}")
 
 if __name__ == "__main__":
     asyncio.run(hello())
