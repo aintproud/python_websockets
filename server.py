cd525deba2553096e8a8bd5d08e23abbf490f5f7
daniilwithangelic <121841013+daniilwithangelic@users.noreply.github.com>
2023-02-15 16:11:22 +1000

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,29 @@
-
 import asyncio
 import websockets
 import json
 
+
 async def hello(websocket):
     async for message in websocket:
         message = json.loads(message)
-        print(message)
-        
-        if message['topic'] == 'name': await websocket.send(f'hi {message["text"]}')
-        elif message['topic'] == 'propose': await websocket.send(f'you are the boss, {message["text"]} means {message["text"]}')
+        # print(message)
 
+        if message['topic'] == 'name':
+            await websocket.send(f'Hi, {message["text"]}!')
+            print(f'>>> Hi {message["text"]}')
+
+        elif message['topic'] == 'propose':
+            await websocket.send(f'You are the boss.{message["text"]},you decide what to do')
+            print(f'>>> You are the boss.{message["text"]},you decide what to do')
+
+        elif message["topic"] == 'news':
+            await websocket.send(f'I am your secretary, who will tell you what to do, if you need it, write. See you soon {message["text"]}!')
+            print(f'>>> am your secretary, who will tell you what to do, if you need it, write. See you soon {message["text"]}!')
 
 async def main():
     async with websockets.serve(hello, "localhost", 8765, ping_timeout=None):
         await asyncio.Future()  # run forever
+
 
 if __name__ == "__main__":
     asyncio.run(main())
