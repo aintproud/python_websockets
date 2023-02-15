3207e048d37c91827b86c4cb0968a0d01f7716e6
daniilwithangelic <121841013+daniilwithangelic@users.noreply.github.com>
2023-02-15 16:22:47 +1000

    Update server.py

--- server.py
+++ server.py
@@ -10,15 +10,21 @@ async def hello(websocket):
 
         if message['topic'] == 'name':
             await websocket.send(f'Hi, {message["text"]}!')
-            print(f'>>> Hi {message["text"]}')
+            print(">>> Write your name:")
+            print(f'<<< {message["text"]}')
+            print(f'>>> Hi {message["text"]}!')
 
         elif message['topic'] == 'propose':
-            await websocket.send(f'You are the boss.{message["text"]},you decide what to do')
+            await websocket.send(f'You are the boss.{message["text"]},you decide what to do!')
+            print(">>> Wanna go out?")
+            print(f'<<< {message["vuv"]}')
             print(f'>>> You are the boss.{message["text"]},you decide what to do')
 
         elif message["topic"] == 'news':
             await websocket.send(f'I am your secretary, who will tell you what to do, if you need it, write. See you soon {message["text"]}!')
-            print(f'>>> am your secretary, who will tell you what to do, if you need it, write. See you soon {message["text"]}!')
+            print(">>> Want to know more?")
+            print(f'<<< {message["drop"]}')
+            print(f'>>> I am your secretary, who will tell you what to do, if you need it, write. See you soon {message["text"]}!')
 
 async def main():
     async with websockets.serve(hello, "localhost", 8765, ping_timeout=None):
